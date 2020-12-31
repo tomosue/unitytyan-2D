4ee00cf529d433fc0251b2ea47396c765414bd6e
Tomosue Toshimasa <nikoniko04211970@gmail.com>
2020-12-31 10:30:56 +0900

    エラーの解消

---
+++ unitytyan 2D/Assets/Scripts/Character/BaseCharacterController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterController : MonoBehaviour
+{
+	public bool isActive = false; 
+
+	[SerializeField]
+	protected int maxHP = 1;
+	[SerializeField]
+	protected float defaultSpeed = 0;
+	[SerializeField]
+	protected int defaultPower = 0;
+	[SerializeField]
+	protected float jumpPower = 0;
+	[SerializeField]
+	protected GameObject[] groundCheckObjects = new GameObject[3];
+
+	protected int hp = 0;
+	protected float speed = 0;
+	protected int power = 0;
+	protected GameObject gameManagerObj;
+	protected GameManager gameManager;
+	protected bool isGrounded = false;
+	protected bool isGroundedPrev = false;
+	protected float direction = 1;
+
+	public int Hp
+	{
+		set
+		{
+			hp = Mathf.Clamp(value, 0, maxHP);
+
+			if(hp <= 0)
+			{
+				Dead();
+			}
+		}
+		get
+		{
+			return hp;
+		}
+	}
+
+	public float Speed
+	{
+		set
+		{
+			speed = value;
+		}
+		get
+		{
+			return speed;
+		}
+	}
+
+	public int Power
+	{
+		set
+		{
+			power = Mathf.Max(value, 0);
+		}
+		get
+		{
+			return power;
+		}
+	}
+
+
+    protected virtual void Start()
+    {
+		gameManagerObj = GameObject.FindGameObjectWithTag("GameController");
+		gameManager = gameManagerObj.GetComponent<GameManager>();
+
+		InitCharacter();
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+
+	protected virtual void FixedUpdate()
+	{
+		FixedUpdateCharacter();
+	}
+
+	protected virtual void FixedUpdateCharacter()
+	{
+
+	}
+
+	protected virtual void InitCharacter()
+	{
+		Hp = maxHP;
+		Speed = defaultSpeed;
+
+		isActive = true;
+	}
+
+	protected virtual void Move()
+	{
+
+	}
+
+	protected virtual void Damage()
+	{
+
+	}
+
+	protected virtual void Dead()
+	{
+
+	}
+	protected virtual void UpdateAnimation()
+	{
+
+	}
+	protected void GroundCheck()
+	{
+		isGroundedPrev = isGrounded;
+		Collider2D[] groundCheckCollider = new Collider2D[groundCheckObjects.Length];
+		//接地判定オブジェクトが何かに重なっているかどうかをチェック
+		for(int i = 0; i < groundCheckObjects.Length; i++)
+		{
+			groundCheckCollider[i] = Physics2D.OverlapPoint(groundCheckObjects[i].transform.position);
+			//接地判定オブジェクトのうち、1つでも何かに重なっていたら接地しているものとして終了
+			if(groundCheckCollider[i] != null)
+			{
+				isGrounded = true;
+				return;
+			}
+		}
+		//ここまできたということは何も重なっていないということなので、接地していないと判断する
+		isGrounded = false;
+	}
+
+}
---
+++ unitytyan 2D/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : BaseCharacterController
+{
+  [SerializeField]
+  string horizontalInputName = "Horizontal";
+  [SerializeField]
+  string jumpButtonName = "Jump";
+  bool jump = false;
+  bool damage = false;
+  float inputH = 0;
+  Animator animator;
+  SpriteRenderer spriteRenderer;
+  Rigidbody2D rigidBody2D;
+  protected override void Start()
+  {
+    base.Start();
+    animator = GetComponent<Animator>();
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    rigidBody2D = GetComponent<Rigidbody2D>();
+  }
+  protected override void Update()
+  {
+    GetInput();
+  }
+  protected override void FixedUpdate()
+  {
+    Move();
+  }
+  void GetInput()
+  {
+    if (!isActive)
+    {
+      return;
+    }
+    inputH = Input.GetAxisRaw(horizontalInputName);
+    jump = Input.GetButtonDown(jumpButtonName);
+  }
+  protected override void Move()
+  {
+    if (!isActive)
+    {
+      return;
+    }
+    //接地判定
+    GroundCheck();
+    //移動速度の計算処理
+    if (inputH != 0)
+    {
+      direction = Mathf.Sign(inputH);
+      speed = defaultSpeed * direction;
+      //入力がマイナスならスプライトの向きを反転させる
+      spriteRenderer.flipX = direction < 0 ? true : false;
+    }
+    else
+    {
+      speed = 0;
+    }
+    //ジャンプの速度計算
+    if (jump && isGrounded)
+    {
+      rigidBody2D.velocity = Vector3.up * jumpPower;
+    }
+    //アニメーションを更新
+    UpdateAnimation();
+    //実際の移動処理
+    rigidBody2D.velocity = new Vector2(speed, rigidBody2D.velocity.y);
+  }
+    
+  protected override void Damage()
+  {
+    
+  }
+  protected override void Dead()
+  {
+    isActive = false;
+  }
+    protected override void UpdateAnimation()
+    {
+      /*animator.SetFloat("Speed", Mathf.Abs(speed));
+      animator.SetBool("Jump", !isGrounded);*/
+    }
+}
